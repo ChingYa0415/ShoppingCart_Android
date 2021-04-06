151b9edf1f95880c1f5ce2eb0e4804e70ed9ce81
DESKTOP-6VV6EPI\Ching <angus09855069@gmail.com>
2021-04-06 15:37:26 +0800

    no message

--- app/src/main/java/com/example/shoppingcart/MainActivity.java
+++ app/src/main/java/com/example/shoppingcart/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.shoppingcart;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "TAG_MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        NavController navController = Navigation.findNavController(this, R.id.main_fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+        
+
     }
 }
--- app/src/main/java/com/example/shoppingcart/Register.java
+++ app/src/main/java/com/example/shoppingcart/Register.java
@@ -59,6 +59,6 @@ public class Register extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        return inflater.inflate(R.layout.fragment_member, container, false);
     }
 }
